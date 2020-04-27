f0d7abb536f5ee0bbca5dc7f9d467a57a238f78e
PitiMonster <piotrszym159@gmail.com>
2020-04-27 15:50:12 +0200

    feature: api authentication and friends

--- Backend/chat/urls.py
+++ Backend/chat/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('contacts/', views.ContactsView.as_view(), name='contacts'),
+    path('conversations/',views.ConversationsView.as_view(), name='conversations'),
 ]
--- Backend/chat/views.py
+++ Backend/chat/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User, Contact, Conversation, UserConversation
+import json
+from django.core import serializers
+from .serializers import ContactSerializer, UserSerializer, UserConversationSerializer
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -7,3 +14,22 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+class ContactsView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'content': ContactSerializer(request.user.contact.get()).data}
+        return Response(content)
+
+
+class ConversationsView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        print(request.user.conversations.all())
+        conversations_data = []
+        for conversation in request.user.conversations.all():
+             conversations_data.append(UserConversation.objects.get(user=request.user, conversation=conversation))
+        content = {'content': UserConversationSerializer(conversations_data, many=True).data}
+        return Response(content)
