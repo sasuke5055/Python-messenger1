fd8669a4ede3946172f56cc789689ec242e05a53
regin <cicheckiigor@gmail.com>
2020-05-05 15:06:29 +0200

    feat: web socket messages

--- Backend/chat/utils.py
+++ Backend/chat/utils.py
@@ -4,13 +4,14 @@ def create_message(author, conversation, content):
   try:
     new_message = Message.objects.create(author=author, content=content, conversation=conversation)
 
-    for participant in conversation.participants:
+    for participant in conversation.participants.all():
       userConversation = UserConversation.objects.get(user=participant, conversation=conversation)
       userConversation.unread = True
     new_message.save()
     return new_message
 
-  except:
+  except Exception as e:
+    print(e)
     return None
   
   
